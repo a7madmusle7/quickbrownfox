d4881c283238079e5e3f71d6ad9483a4014036a9
a7madmusle7 <amusleh1W@cnm.edu>
2019-05-20 14:44:55 -0600

    Strings and Arrays and Stuff

---
+++ src/edu/cnm/deepdive/ArraysAndStrings/arraysandstuff.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive.ArraysAndStrings;
+
+public class arraysandstuff {
+
+    public static void main(String[] args) {
+
+      for (int i = 0; i < args.length; i++) {
+        System.out.println(i + ": " + args[i].toUpperCase());
+      }
+
+    }
+
+  }
+
+
+
